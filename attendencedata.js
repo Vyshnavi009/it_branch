1cc49c19ac9e8f65bf792135722756ae06cad1e1
P.Vyshnavi <94984934+Vyshnavi009@users.noreply.github.com>
2023-01-13 20:20:33 +0530

    Update attendencedata.js

--- attendencedata.js
+++ attendencedata.js
@@ -1,17 +1,1 @@
-const mongoose=require('mongoose')
 
-const schema = mongoose.Schema;
-
-const attendenceSchema = new schema({
-    Username:{
-        type:String,
-        unique:true,
-        required:true
-    },
-    attendence:{
-        type:String,
-        required:true
-    }
-})
-
-module.exports = mongoose.model('studentattendence',attendenceSchema)
